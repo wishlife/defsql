a493d75c319951c32bb29744f1f7d42c6394b58f
Lei <tendant@gmail.com>
2015-11-19 00:20:25 -0800

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject defsql "0.1.1"
+(defproject defsql "0.1.2-SNAPSHOT"
   :description "A Clojure library designed to make your life easier with SQL Database using JDBC."
   :url "https://github.com/wishlife/defsql"
   :license {:name "Eclipse Public License"
