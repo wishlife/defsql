8a3e2147b57375f307dc16add93d636b2a4b8458
Lei Wang <tendant@gmail.com>
2020-02-13 10:05:20 -0800

    Release 0.2.0 with breaking change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject defsql "0.1.8-SNAPSHOT"
+(defproject defsql "0.2.0"
   :description "A Clojure library designed to make your life easier with SQL Database using JDBC."
   :url "https://github.com/wishlife/defsql"
   :license {:name "Eclipse Public License"
